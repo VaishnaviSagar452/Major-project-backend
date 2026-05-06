fe6326de4babd115b67b1b0069d2584ec4578765
VaishnaviSagar452 <vaish08sagar@gmail.com>
2026-05-06 08:46:54 +0530

    fixed bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,15 +6,15 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser= asyncHandler(async(req,res)=>{
     // Implementation for registering user
-     const {Fullname,email,username, password}= req.body
+     const {fullname,email,username, password}= req.body
      console.log("email", email)
 
      if(
-        [Fullname,email,username,password].some((field)=> field?.trim() === "")
+        [fullname,email,username,password].some((field)=> field?.trim() === "")
      ){
         throw new ApiError(400,"All fields are required")
      }
-     const existedUser =User.findOne({
+     const existedUser =await User.findOne({
           $or:[
             {email},
             {username}
@@ -24,14 +24,14 @@ const registerUser= asyncHandler(async(req,res)=>{
         throw new ApiError(409, "User with this email or username already exists")
      }
        const avatarLocalPath = req.files?.avatar[0]?.path;
-       const coverImageLocalPath =req.files?.coverImage[0]?.path;
+       const coverImageLocalPath =req.files?.coverImage?.[0]?.path;
 
        if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
        }
 
-      const avatar = await UploadOnCloudinary(avatarLocalPath)
-      const coverImage = await UploadOnCloudinary(coverImageLocalPath)
+      const avatar = await uploadOnCloudinary(avatarLocalPath)
+      const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
       if(!avatar){
          throw new ApiError(400, "Error while uploading avatar")
@@ -43,7 +43,7 @@ const registerUser= asyncHandler(async(req,res)=>{
             coverImage: coverImage?.url || "",
             email,
             password,
-            username : username.toLowercase()
+            username : username.toLowerCase()
       })
 
       const createdUser = await User.findById(user._id).select(
